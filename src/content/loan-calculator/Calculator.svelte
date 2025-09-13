81e0012a9ef44ed7c9355420231a43372b14999e
Logan Roberts <logan@lcroberts.dev>
2025-09-13 12:22:53 -0400

    Get some calculations working

--- src/content/loan-calculator/Calculator.svelte
+++ src/content/loan-calculator/Calculator.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
-  let amount: Number | null | undefined = $state(null);
+  import { totalMontlyPayment, formatCurrency } from "./functions";
+
+  let amount: number = $state(0);
+  let term: number = $state(0);
+  let interestPercentage: number = $state(0);
+  let termMultiplier = $state(12);
+  const interest = $derived((interestPercentage || 0) / 100);
+  const montlyPayment = $derived.by(() => {
+    if (amount && term && interestPercentage) {
+      return totalMontlyPayment(amount, interest / 12, term * termMultiplier);
+    }
+    return 0;
+  });
 </script>
 
 <div class="w-full">
-  <input bind:value={amount} type="number" />
-  {#if amount}
-    not null
-  {/if}
+  <label for="loan-amount">Loan Amount:</label>
+  <input id="loan-amount" bind:value={amount} type="number" />
+  <label for="loan-term">Loan Term:</label>
+  <div class="flex gap-2">
+    <input id="loan-term" bind:value={term} type="number" />
+    <select id="term-multiplier" bind:value={termMultiplier}>
+      <option value={12}>Years</option>
+      <option value={1}>Months</option>
+    </select>
+  </div>
+  <label for="loan-interest">Yearly Interest Rate (%):</label>
+  <input id="loan-interest" bind:value={interestPercentage} type="number" />
+  <div>Monthly Payment: {formatCurrency(montlyPayment)}</div>
 </div>
---
+++ src/content/loan-calculator/functions.ts
@@ -0,0 +1,22 @@
+export function totalMontlyPayment(loanAmount: number, termInterest: number, numberOfTerms: number): number {
+  return bankersRound((loanAmount * termInterest * Math.pow(1 + termInterest, numberOfTerms)) / (Math.pow(1 + termInterest, numberOfTerms) - 1));
+}
+
+export function principlePayment(totalMonthly: number, outstandingBalance: number, termInterest: number): number {
+  return bankersRound(totalMonthly - outstandingBalance / termInterest);
+}
+
+export function bankersRound(amount: number, decimalPlaces: number = 2): number {
+  const placeAdjusted = amount * Math.pow(10, decimalPlaces);
+  const rounded = Math.round(placeAdjusted);
+  var roundedPlaceAdjusted = Math.abs(placeAdjusted) % 1 === 0.5 ? (rounded % 2 === 0 ? rounded : rounded - 1) : rounded;
+  return roundedPlaceAdjusted / Math.pow(10, decimalPlaces);
+}
+
+export function formatCurrency(amount: number): string {
+  return Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    trailingZeroDisplay: 'stripIfInteger',
+  }).format(amount);
+}
