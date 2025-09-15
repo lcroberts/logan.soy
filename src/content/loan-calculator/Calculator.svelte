23f5ecd46ef1444c540b99930e94cc3080ad4840
Logan Roberts <logan@lcroberts.dev>
2025-09-14 21:00:26 -0400

    Create chartjs attachment

--- src/content/loan-calculator/Calculator.svelte
+++ src/content/loan-calculator/Calculator.svelte
@@ -1,6 +1,12 @@
+<script module lang="ts">
+  import { CategoryScale, Chart, LinearScale, LineController, LineElement, PointElement } from "chart.js";
+  Chart.register(LineController, CategoryScale, LinearScale, PointElement, LineElement);
+</script>
+
 <script lang="ts">
   import NumberInput from "../../components/svelte/NumberInput.svelte";
   import { totalMontlyPayment, formatCurrency } from "./functions";
+  import type { Attachment } from "svelte/attachments";
 
   let amount: number = $state(1000);
   let term: number = $state(1);
@@ -13,12 +19,29 @@
     }
     return 0;
   });
+
+  const chartJsAttachment: Attachment = (element) => {
+    const chart = new Chart(element as HTMLCanvasElement, {
+      type: "line",
+      data: {
+        labels: [1, 2, 3, 4, 5],
+        datasets: [
+          {
+            data: [100, 200, 300, 400, amount],
+          },
+        ],
+      },
+    });
+    return () => {
+      chart.destroy();
+    };
+  };
 </script>
 
 <div class="w-full">
   <label for="loan-amount">Loan Amount:</label>
   <div class="input w-fit">
-    $ <NumberInput id="loan-interest" bind:value={amount} class="no-style" min="1" allowNegative={false}/>
+    $ <NumberInput id="loan-amount" bind:value={amount} class="no-style" min="1" allowNegative={false} />
   </div>
   <label for="loan-term">Loan Term:</label>
   <div class="flex gap-2">
@@ -33,4 +56,5 @@
     <NumberInput id="loan-interest" bind:value={interestPercentage} maxDecimal={5} allowNegative={false} class="no-style w-20" /> %
   </div>
   <div>Monthly Payment: {formatCurrency(montlyPayment)}</div>
+  <canvas {@attach chartJsAttachment}></canvas>
 </div>
